f4481cc8fed9487ccb8b0d9fcd5802ca037eb003
Krasilnikov Roman <x0kill@ya.ru>
2024-06-19 19:56:08 +0300

    `&str` for strings

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,63 @@
 use std::collections::{HashMap, HashSet};
 
 #[derive(Debug)]
-enum Node {
-    Content(String),
+enum Node<'a> {
+    Content(&'a str),
     Segment {
-        name: String,
-        content: String,
-        dependencies: Vec<String>,
+        name: &'a str,
+        content: &'a str,
+        dependencies: Vec<&'a str>,
     },
 }
 
-fn make_graph(nodes: &Vec<Node>) -> HashMap<String, HashSet<String>> {
-    let mut graph: HashMap<String, HashSet<String>> = HashMap::new();
+fn make_graph<'a>(nodes: &Vec<Node<'a>>) -> HashMap<&'a str, HashSet<&'a str>> {
+    let mut graph: HashMap<&'a str, HashSet<&'a str>> = HashMap::new();
     for node in nodes {
         match node {
             Node::Content(_) => {}
             Node::Segment {
                 name, dependencies, ..
             } => {
-                graph.insert(name.clone(), dependencies.clone().into_iter().collect());
+                graph.insert(name, dependencies.iter().cloned().collect());
             }
         }
     }
     graph
 }
 
-fn resolve_target(nodes: &Vec<Node>, target: &str) -> HashSet<String> {
+fn resolve_target<'a>(nodes: &Vec<Node<'a>>, target: &'a str) -> HashSet<&'a str> {
     let graph = make_graph(nodes);
     let mut visited = HashSet::new();
 
     let mut stack = Vec::new();
-    stack.push(target.to_string());
+    stack.push(target);
 
     while let Some(node) = stack.pop() {
         if visited.contains(&node) {
             continue;
         }
         visited.insert(node.clone());
-        if let Some(neighbors) = graph.get(&node) {
-            stack.extend(neighbors.iter().cloned());
+        if let Some(deps) = graph.get(node) {
+            stack.extend(deps.iter());
         }
     }
     visited
 }
 
-fn resolve(target: String, nodes: Vec<Node>) -> String {
+fn resolve(target: &str, nodes: Vec<Node>) -> String {
     let segments = resolve_target(&nodes, &target);
-    let mut result = String::new();
+    let mut result = Vec::new();
     for node in nodes {
         match node {
-            Node::Content(content) => {
-                if !result.is_empty() {
-                    result.push_str("\n");
-                }
-                result.push_str(&content)
-            }
+            Node::Content(content) => result.push(content),
             Node::Segment { name, content, .. } => {
-                if !result.is_empty() {
-                    result.push_str("\n");
-                }
                 if segments.contains(&name) {
-                    result.push_str(&content);
+                    result.push(content)
                 }
             }
         }
     }
-    result
+    result.join("\n")
 }
 
 #[cfg(test)]
@@ -74,69 +66,63 @@ mod tests {
 
     #[test]
     fn should_resolve_common_content() {
-        let nodes = vec![Node::Content("common content".to_string())];
-        assert_eq!(resolve("".to_string(), nodes), "common content");
+        let nodes = vec![Node::Content("common content")];
+        assert_eq!(resolve("", nodes), "common content");
     }
 
     #[test]
     fn should_resolve_segment_content() {
         let nodes = vec![Node::Segment {
-            name: "foo".to_string(),
-            content: "foo content".to_string(),
+            name: "foo",
+            content: "foo content",
             dependencies: Vec::new(),
         }];
-        assert_eq!(resolve("foo".to_string(), nodes), "foo content");
+        assert_eq!(resolve("foo", nodes), "foo content");
     }
 
     #[test]
     fn should_resolve_all_content() {
         let nodes = vec![
-            Node::Content("common content".to_string()),
+            Node::Content("common content"),
             Node::Segment {
-                name: "foo".to_string(),
-                content: "foo content".to_string(),
+                name: "foo",
+                content: "foo content",
                 dependencies: Vec::new(),
             },
         ];
-        assert_eq!(
-            resolve("foo".to_string(), nodes),
-            "common content\nfoo content"
-        );
+        assert_eq!(resolve("foo", nodes), "common content\nfoo content");
     }
 
     #[test]
     fn should_resolve_dependency() {
         let nodes = vec![
             Node::Segment {
-                name: "foo".to_string(),
-                content: "foo content".to_string(),
+                name: "foo",
+                content: "foo content",
                 dependencies: Vec::new(),
             },
             Node::Segment {
-                name: "bar".to_string(),
-                content: "bar content".to_string(),
-                dependencies: vec!["foo".to_string()],
+                name: "bar",
+                content: "bar content",
+                dependencies: vec!["foo"],
             },
         ];
-        assert_eq!(
-            resolve("bar".to_string(), nodes),
-            "foo content\nbar content"
-        );
+        assert_eq!(resolve("bar", nodes), "foo content\nbar content");
     }
 }
 
 fn main() {
     let nodes = vec![
-        Node::Content("common content".to_string()),
+        Node::Content("common content"),
         Node::Segment {
-            name: "foo".to_string(),
-            content: "foo content".to_string(),
+            name: "foo",
+            content: "foo content",
             dependencies: Vec::new(),
         },
         Node::Segment {
-            name: "bar".to_string(),
-            content: "bar content".to_string(),
-            dependencies: vec!["foo".to_string()],
+            name: "bar",
+            content: "bar content",
+            dependencies: vec!["foo"],
         },
     ];
 
